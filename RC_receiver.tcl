91b9e0fabffd7e06d9e1c8530483a6d3f9d024bc
Joseph Fusco <jwfusco@me.com>
2021-12-07 20:55:17 -0500

    oops

--- RC_receiver.tcl
+++ RC_receiver.tcl
@@ -10,11 +10,11 @@ vmap work work
 # compile the program and test-bench files
 vcom ../sim_mem_init/sim_mem_init.vhd
 vcom ../hex_to_7_seg/hex_to_7_seg.vhd
-vcom RC_receiver_student.vhd
+vcom RC_receiver_students.vhd
 vcom test_RC_receiver.vhd 
 
 # initializing the simulation window and adding waves to the simulation window
-vsim test_RC_receiver_student
+vsim test_RC_receiver_students
 add wave sim:/test_RC_receiver/dev_to_test/*
  
 # define simulation time
